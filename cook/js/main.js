7453fe4d71654af7d480aef05cade98ef8c7fe4a
Simon Altschuler <simon@altschuler.dk>
2013-01-02 13:14:47 +0100

    basic test implemented

--- cook/js/main.js
+++ cook/js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+    var saved = $.cookie('selected-thing');
+    if (saved !== null)
+	$('.sel').val(saved);
 
+    console.log(saved);
+    $('.sel').on('change', function() {
+	console.log($(this).val());
+	$.cookie('selected-thing', $(this).val(), {path:'/'});
+    });
+});
